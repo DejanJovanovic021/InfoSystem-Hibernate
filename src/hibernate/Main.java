006136d9a9407da172162d3fb04835262fabcaa3
DejanJovanovic021 <dejan.jovanovic.java021@gmail.com>
2021-05-03 21:11:06 +0200

    Java Swing, MySQL, Hibernate

---
+++ src/hibernate/Main.java
@@ -0,0 +1,16 @@
+
+package hibernate;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Application app = new Application();
+        app.setVisible(true);
+        
+        
+    }
+    
+}
